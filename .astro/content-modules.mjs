7b8ce7e779aa26b0fa7312e7feb259aaf008828d
alex <alexnawotka@pm.me>
2025-12-05 04:44:41 -0500

    [UPD]

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,8 +2,8 @@
 export default new Map([
 ["src/content/page/About.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FAbout.mdx&astroContentModuleFlag=true")],
 ["src/content/page/Contact.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FContact.mdx&astroContentModuleFlag=true")],
-["src/content/page/Drawings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FDrawings.mdx&astroContentModuleFlag=true")],
 ["src/content/page/Paintings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FPaintings.mdx&astroContentModuleFlag=true")],
 ["src/content/page/Home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FHome.mdx&astroContentModuleFlag=true")],
+["src/content/page/Drawings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FDrawings.mdx&astroContentModuleFlag=true")],
 ["src/content/art/suspended.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fart%2Fsuspended.mdx&astroContentModuleFlag=true")]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,10 @@ import favicons from 'astro-favicons';
 
 
 export default defineConfig({
-  site: 'https://cassandraopeia.com',
+  site: 'https://liminalfun.github.io/',
+  base: '/cassandraopeia',
+
+  // site: 'https://cassandraopeia.com',
 
   redirects: {
       // '/blog/[...slug]': '/writing/[...slug]'
