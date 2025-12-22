70a57ad8b79fce1f9ad8ca64d6a8386432d32425
alex <alexnawotka@pm.me>
2025-12-21 20:57:28 -0500

    [ADD] art page description + 404

--- src/middleware.mjs
+++
@@ -1,7 +0,0 @@
-export const onRequest = async (context, next) => {
-    const response = await next();
-    if (response.status === 404) {
-        return context.rewrite("/");
-    }
-    return response;
-};
